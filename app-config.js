0802d7dba46ead83709ede75c3c190341c49088d
oyakodon2 <mesuzuki@j-asa.net>
2026-03-02 17:45:42 +0900

    Create app-config.js

---
+++ app-config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  // Supabase Project URL (例: https://xxxx.supabase.co)
+  supabaseUrl: 'https://zhjnfoliacbhkatdjaew.supabase.co',
+  // Supabase anon public key
+  supabaseAnonKey: 'sb_publishable_PGIAoYw1neIv_FzxWrOsTA_MZ6BQIvO',
+};
